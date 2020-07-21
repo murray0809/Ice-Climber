1de7536e68858a2b3dd393c6332a28eaa43dc1d3
rutu009 <8901diowryyy@gmail.com>
2020-07-21 22:52:34 +0900

    SourceTreeのテスト用です気にしないでください

---
+++ Assets/Script/TestOkawa.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestOkawa : MonoBehaviour
+{
+    // Start is called before the first frame update
+    //SourceTree用のテストフォルダです気にしないでください
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
