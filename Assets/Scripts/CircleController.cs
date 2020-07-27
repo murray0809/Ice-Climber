0cf3dd2359e0a4e78d487c32cce1d23a625ff06a
tazaki292 <kouta.clarinet@icloud.com>
2020-07-27 22:05:56 +0900

    ブロック作成

---
+++ Assets/Scripts/CircleController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CircleController : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.velocity = new Vector2(0, 20f);
+    }
+}
