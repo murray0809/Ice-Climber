fecd46f3305fb445bbd50029a0c2d312f6fb1209
rutu009 <8901diowryyy@gmail.com>
2020-07-27 12:18:39 +0900

    enemy test sceneを追加しました enemtctr scriptを追加しました spriteを追加しました

---
+++ Assets/Script/Enemyctr.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemyctr : MonoBehaviour
+{
+    public float moveSpeed = 0f;
+    Rigidbody2D rbd;
+    GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rbd = GetComponent<Rigidbody2D>();
+         player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 dir = player.transform.position - this.transform.position;
+        dir = dir.normalized;
+
+        // プレイヤーに向かって飛ばす
+        rbd.velocity = dir * moveSpeed;
+    }
+}
