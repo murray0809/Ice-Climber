17a3f67ae2fb176e3fc1c63f8e884da23bfb8892
murray0809 <tomo19940809@gmail.com>
2020-08-09 18:41:24 +0900

    プレイヤーの上左右の攻撃実装

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -11,9 +11,13 @@ public class PlayerController : MonoBehaviour
 
     [SerializeField] float attackTime = 1f;
 
-    bool jump = false;
+    bool jump = false; //ジャンプの接地判定
+    bool right; //右を向いているか
+    bool left; //左を向いているか
 
     [SerializeField] GameObject attack = default;
+    [SerializeField] GameObject attackRight = default;
+    [SerializeField] GameObject attackLeft = default;
 
     Rigidbody2D m_rb;
     PhotonView m_view;
@@ -22,6 +26,8 @@ public class PlayerController : MonoBehaviour
         m_view = GetComponent<PhotonView>();
 
         attack = GameObject.Find("Attack");
+        attackRight = GameObject.Find("Attack_Right");
+        attackLeft = GameObject.Find("Attack_Left");
         attackTime = 0f;
 
         if (m_view)
@@ -33,19 +39,34 @@ public class PlayerController : MonoBehaviour
             }
         }
 
+        right = true;
+
         attack.SetActive(false);
+        attackRight.SetActive(false);
+        attackLeft.SetActive(false);
     }
     void Update()
     {
         if (!m_view.IsMine) return;
 
         float h = Input.GetAxisRaw("Horizontal");
-        
+
         Vector2 vel = m_rb.velocity;
         vel.x = h * moveSpeed;
         m_rb.velocity = vel;
 
         attackTime -= 0.1f;
+
+        if (h > 0)
+        {
+            right = true;
+            left = false;
+        }
+        else if (h < 0)
+        {
+            right = false;
+            left = true;
+        }
 
         if (!jump && Input.GetButtonDown("Jump"))
         { 
@@ -53,15 +74,29 @@ public class PlayerController : MonoBehaviour
             jump = true;
         }
 
-        if (Input.GetButtonDown("Fire1"))
+        if (jump && Input.GetButtonDown("Fire1"))
         {
             attack.SetActive(true);
+            attackTime = 1f;
+            Debug.Log("攻撃した");
+        }
+        if (right && Input.GetButtonDown("Fire1"))
+        {
+            attackRight.SetActive(true);
+            attackTime = 1f;
+            Debug.Log("攻撃した");
+        }
+        if (left && Input.GetButtonDown("Fire1"))
+        {
+            attackLeft.SetActive(true);
             attackTime = 1f;
             Debug.Log("攻撃した");
         }
         if (attackTime <= 0)
         {
             attack.SetActive(false);
+            attackRight.SetActive(false);
+            attackLeft.SetActive(false);
         }
     }
 
