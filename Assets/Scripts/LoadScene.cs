ccea0810235f0b4d5d009343bd0cc076a878127a
rutu009 <8901diowryyy@gmail.com>
2020-10-19 12:03:40 +0900

    TimerScriptを改修しました LoadSceneScriptを作りました

---
+++ Assets/Scripts/LoadScene.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadScene : MonoBehaviour
+{
+    [SerializeField] float fadeTime;
+    public void Load(string loadScene)
+    {
+        Initiate.Fade(loadScene, Color.black, fadeTime);
+    } 
+}
