0e72d7484f4ed03c650b9ed529faf8dd4b9f7cda
rutu009 <8901diowryyy@gmail.com>
2020-11-16 12:46:32 +0900

    NamePlateのスクリプト追加

---
+++ Assets/Scripts/NameCtr.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class NameCtr : MonoBehaviour
+{
+    [SerializeField] Text m_text;
+    [SerializeField] InputField m_inputField;
+    private string m_name;
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+    private void Start()
+    {
+        m_inputField = m_inputField.GetComponent<InputField>();
+        m_text = m_text.GetComponent<Text>();
+        //SceneManager.sceneUnloaded += SceneUnloaded;
+    }
+
+    private void Update()
+    {
+        //Debug.Log(m_name);
+    }
+    public void InputText()
+    {
+        Debug.Log(m_inputField.text);
+        //m_text.text = m_inputField.text;
+        //string name = m_inputField.text;
+    }
+
+    public void EndText()
+    {
+        m_text.text = m_inputField.text;
+        m_name = m_text.text;
+        Debug.Log(m_name);
+        Debug.Log("入力終わり");
+        m_inputField.text = "";
+    }
+}
